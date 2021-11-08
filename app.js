a0152697f17f5a06ced1a2b87c88652a362a0bda
Mircea Salan <msalan-ext@kds.com>
2021-11-08 18:43:58 +0200

    Remove unused comment

--- app.js
+++ app.js
@@ -5,5 +5,3 @@ let cryptoAlgorithm = new CryptoAlgorithm();
 //let runIntervalInMS = process.env.RUN_INTERVAL
 let runIntervalInSeconds = process.env.RUN_INTERVAL_SECONDS
 setTimeout(cryptoAlgorithm.runOnce, runIntervalInSeconds*1000);
-
-//test commit Mircea
